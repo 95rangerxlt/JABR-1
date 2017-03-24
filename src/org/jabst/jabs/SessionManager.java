0f0a02e3d47a75504adf48893803282c82be6fc6
Jason Bigelow <s3606501@student.rmit.edu.au>
2017-03-24 15:26:16 +1100

    Move files to right directory.

--- src/org/jabst/jabs/SessionManager.java
+++ src/org/jabst/jabs/SessionManager.java
@@ -1,37 +1,64 @@
+//import javafx.application.Application;
 
 public class SessionManager {
 
-	//Variables
 	
-	private String username; //parse from GUI?
-	private String password; //parse from GUI?
 	//Initialise the database
-	//DatabaseManager dbm = new DatabaseManager();
+	public dbmStub dbm = new dbmStub();
 	
 	
 	//Methods
 	
-	public boolean session()
-	{
-		if (requestUserCredentialCheck(username, password) == true)
-		{
-			// Create either CustomerUser or BusinessUser object
-			// Initialise appropriate menu for new user object (Integration with GUI)
-			// Populate business objects if CustomerUser is created
+	public boolean checkUser(String username, String password){
+		if (dbm.checkUser(username, password) == true){
 			return true;
 		}
-		else
-		{
+		else{
 			return false;
 		}
-			
+	}
+	
+	public void checkUserType(String username){
+		if (dbm.checkUserType(username) == "Customer"){
+			populateCustomer(username);
+			// TODO: Here the default business will have to be created as an object.
+			launchCustomerMenu();
+		}
+		if (dbm.checkUserType(username) == "Business"){
+			populateBusiness(username);
+			launchBusinessMenu();
+		}
 		
 	}
 	
-	public boolean requestUserCredentialCheck(String username, String password)
-	{
-		//dbm.RequestUserCredentialCheck(username, password);
-		return true; //stub
-		
+	public boolean registerUser(String username, String password, String name, String address, String phone){
+		// Query Database for existing user
+		// populate database
+		return true;
 	}
+	
+	public void populateCustomer(String username){
+		// Request object data from DatabaseManager
+		// create new customer object
+	}
+	
+	public void populateBusiness(String username){
+		// Request object data from DatabaseManager
+		// create new business object
+	}
+	
+	void swapToRegisterWindow(String username, String password){
+		//String[] args = new String[]{username, password};
+		//Application.launch(Login.class, args);
+	}
+	
+	void launchCustomerMenu(){
+		//Application.launch(CustomerMenu.class);
+	}
+	
+	void launchBusinessMenu(){
+		//Application.launch(BusinessMenu.class);
+	}
+	
+	
 }
