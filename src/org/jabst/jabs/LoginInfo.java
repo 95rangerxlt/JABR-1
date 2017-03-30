b5cafeffc408dc97fba77ed053124f7407cdc6aa
Angel English <s3543536@student.rmit.edu.au>
2017-03-30 23:55:20 +1100

    new system for managing windows, classes RegisterInfo and LoginInfo carry information for the Register and Login windows

---
+++ src/org/jabst/jabs/LoginInfo.java
@@ -0,0 +1,13 @@
+package org.jabst.jabs;
+
+public class LoginInfo {
+
+	public static String username;
+	public static String password;
+
+	public static enum Buttons {
+		LOGIN, REGISTER
+	}
+
+	public static Buttons button;
+}
---
+++ src/org/jabst/jabs/RegisterInfo.java
@@ -0,0 +1,17 @@
+package org.jabst.jabs;
+
+public class RegisterInfo {
+
+	public static String username;
+	public static String password;
+
+	public static enum Buttons {
+		LOGIN, REGISTER
+	}
+
+	public static Buttons button;
+
+	public static String name;
+	public static String address;
+	public static String phone;
+}
