2812e3d48afac98458db0157102f5943334e94e6
Jason Bigelow <s3606501@student.rmit.edu.au>
2017-04-26 09:46:55 +1000

    Partial progress: New class DateTableConversion

--- src/org/jabst/jabs/Employee.java
+++ src/org/jabst/jabs/Employee.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.jabst.jabs.util.DateTableConversion;
+
 public class Employee {
 
 	public long id;
@@ -29,8 +31,8 @@ public class Employee {
 	}
 
 
-	public void createDatesFromTable() {
-		workingHours = new ArrayList<WeekDate>();//reset data
+	public static ArrayList<Date> createDatesFromTable(Timetable table, Calendar startDate) {
+		ArrayList<Date> dates = new ArrayList<Date>();
 
 		if(table.table.size() > 0) {//make sure there is data
 			// nested loops for nested arrays
@@ -39,90 +41,26 @@ public class Employee {
 
 					if(table.table.get(i).get(j) == Timetable.CellStatus.BOOKED_BY_YOU) {
 						// create date
-						//TODO: Change from Calendar conversion to WeekDay conversion
-						WeekDate timeSlot = (Calendar)startDate.clone();
+						Calendar timeSlot = (Calendar)startDate.clone();
 						timeSlot.add(Calendar.DAY_OF_YEAR, i);
 						timeSlot.add(Calendar.HOUR_OF_DAY, j);
-						workingHours.add(timeSlot.getTime());
+						dates.add(timeSlot.getTime());
 					}
 				}
 			}
 		}
+		return dates;
 	}
 
 	public Timetable createTableFromDates() {
-		table = new Timetable(true);
-		System.out.println("creating table from " + workingHours.size() + " shifts");
-		ArrayList<Calendar> hoursCalendar = getCalendars(workingHours);
-		Calendar min = minDate(hoursCalendar);
-		startDate = (Calendar)min.clone();
-		Calendar max = maxDate(hoursCalendar);
-		int days = max.get(Calendar.DAY_OF_YEAR) - min.get(Calendar.DAY_OF_YEAR);
-		table.createBlankTables();
-
-		// fill tables
-		for(int i = 0; i < hoursCalendar.size(); i++) {
-
-			System.out.println("i="+i);
-			System.out.println("row index:");
-			System.out.println("hoursCalendar.get(i).get(Calendar.DAY_OF_YEAR) - min.get(Calendar.DAY_OF_YEAR) = "
-				+hoursCalendar.get(i).get(Calendar.DAY_OF_YEAR) +"-"
-				+min.get(Calendar.DAY_OF_YEAR)+"="
-				+(hoursCalendar.get(i).get(Calendar.DAY_OF_YEAR)-min.get(Calendar.DAY_OF_YEAR))
-			);
-			int getting = (hoursCalendar.get(i).get(Calendar.DAY_OF_YEAR) - min.get(Calendar.DAY_OF_YEAR));
-			
-			int cellIdx = hoursCalendar.get(i).get(Calendar.HOUR_OF_DAY)-startingHour;
-			
-			if (cellIdx < 0 || cellIdx > hoursInADay) {
-				System.out.println(
-					"Discarding out of hours entry for employee hours:"
-					+hoursCalendar.get(i).toString()
-				);
-				continue;
-			}
-			System.out.println("setting cell: " + cellIdx);
-			table.table.get(getting).set(cellIdx, Timetable.CellStatus.BOOKED_BY_YOU);
-		}
-		System.out.println("end of createTableFromDates");
-		// TODO: clean up all these println statements
-		return table;
-	}
-
-	private ArrayList<Calendar> getCalendars(ArrayList<WeekDate> list) {
-		ArrayList<Calendar> newList = new ArrayList<Calendar>();
-		for(int i = 0; i < list.size(); i++) {
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(list.get(i));
-			newList.add(cal);
-		}
-		return newList;
-	}
-	
-
-	private Calendar minDate(ArrayList<Calendar> list) {
-		System.out.println("minDate:\n\tlist size: " + list.size());
-		if(list.size() > 0) {
-			Calendar min = list.get(0);
-			for(int i = 0; i < list.size(); i++) {
-				if(list.get(i).compareTo(min) < 0) {
-					min = list.get(i);
-				}
-			}
-			return min;
-		} else {
-			return null;
-		}
-	}
-
-	private Calendar maxDate(ArrayList<Calendar> list) {
-		Calendar max = list.get(0);
-		for(int i = 0; i < list.size(); i++) {
-			if(list.get(i).compareTo(max) > 0) {
-				max = list.get(i);
-			}
-		}
-		return max;
+		// FIXME: Temporarily using appointments
+		// instead of working hours so it compiles
+		return DateTableConversion.createTableFromDates(
+            appointmentHours,
+            startDate,
+            hoursInADay,
+            startingHour
+        );
 	}
 
 	public String getName() {
