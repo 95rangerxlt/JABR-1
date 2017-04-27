57300a99f415d4edd171793dab174f01e270d9cc
Jason Bigelow <s3606501@student.rmit.edu.au>
2017-04-27 10:59:33 +1000

    Calendar <-> DayOfWeek conversions

--- src/org/jabst/jabs/util/DayOfWeekConversion.java
+++ src/org/jabst/jabs/util/DayOfWeekConversion.java
@@ -2,9 +2,10 @@ package org.jabst.jabs.util;
 
 import java.time.DayOfWeek;
 import java.util.Calendar;
+import org.jabst.jabs.WeekDate;
 
 /** Static methods for converting between DayOfWeek and Calendar representations
-  * of the day of week
+  * of the day of week, and between Calendar objects and day of week objects
   */
 public class DayOfWeekConversion {
 
@@ -27,6 +28,39 @@ public class DayOfWeekConversion {
         dow += 1;
         dow = (dow == 8 ? 1 : dow);
         return dow;
+    }
+    
+    /** Converts a WeekDate into a Calendar for this week
+      * @param wd The WeekDate to convert to a calendar
+      * @return A Calendar which is set to the day of week and time of day
+      * in the WeekDate, and to this week in absolute value.
+      */
+    public static Calendar wd2cal (WeekDate wd) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR, wd.getStartingHour());
+        c.set(Calendar.MINUTE, (wd.getTime()/60)%60);
+        c.set(Calendar.SECOND, wd.getTime()%60);
+        c.set(Calendar.MILLISECOND, 0);
+        c.set(Calendar.DAY_OF_WEEK,
+            DayOfWeekConversion.dow2cal(wd.getDayOfWeek().getValue()));
+        return c;
+    }
+    
+    /** Converts a Calendar into the corresponding WeekDate
+      * @param c The Calendar to be converted
+      * @return A WeekDate with the time of day and day of week
+      * found in the Calendar.
+      */
+    public static WeekDate cal2wd (Calendar c) {
+        /* WeekDate(DayOfWeek, timeOfDay */
+        return new WeekDate (
+            DayOfWeek.of(
+                DayOfWeekConversion.cal2dow(c.get(Calendar.DAY_OF_WEEK))
+            ),
+              c.get(Calendar.HOUR)*3600
+            + c.get(Calendar.MINUTE)*60
+            + c.get(Calendar.SECOND)
+        );
     }
 
     /** Main is used to test this class */
