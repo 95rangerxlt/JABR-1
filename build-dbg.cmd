65d13fad80d5506434503642b2143758cf82b2cd
Jason Bigelow <s3606501@student.rmit.edu.au>
2017-05-26 11:12:22 +1000

    Update shellscript for debug builds

--- build-dbg.cmd
+++ build-dbg.cmd
@@ -3,7 +3,7 @@
 call detect.cmd
 
 rem Compilation
-javac -g -d bin -classpath .\lib\hsqldb.jar -sourcepath src src\org\jabst\jabs\*.java
+javac -g -d bin -classpath .\lib\hsqldb.jar -sourcepath src\main\java src\main\java\org\jabst\jabs\*.java
 jar cfm jabs.jar MANIFEST.MF -C bin .
 move jabs.jar deployment
 copy lib\hsqldb.jar deployment\hsqldb.jar
